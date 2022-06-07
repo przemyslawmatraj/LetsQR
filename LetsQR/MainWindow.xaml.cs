ccae8b72ea786e161d8918005c2ab6090197842d
przemyslawmatraj <przemyslawmatraj@PRZEMYSAWMAEFD0>
2022-06-07 15:44:40 +0200

    Change concept

--- LetsQR/MainWindow.xaml.cs
+++ LetsQR/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TransLATTE
+namespace LetsQR
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
