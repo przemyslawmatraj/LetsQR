e5775b3598cfc9326a2c70b59bf79c23879f03e3
przemyslawmatraj <przem.matt@gmail.com>
2022-06-07 22:51:23 +0200

    Add scanning QR code functionality

--- LetsQR/ScanQR.xaml.cs
+++ LetsQR/ScanQR.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using Microsoft.Win32;
 
 namespace LetsQR
 {
@@ -20,9 +24,22 @@ namespace LetsQR
     /// </summary>
     public partial class ScanQR : UserControl
     {
+        QRCodeDecoder decoder = new QRCodeDecoder();
+        OpenFileDialog openFileDialog = new OpenFileDialog();
         public ScanQR()
         {
             InitializeComponent();
+        }
+
+        private void scanBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == true)
+            {
+                QRCodeImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                string decoded = decoder.Decode(new QRCodeBitmapImage(new Bitmap(openFileDialog.FileName)));
+                resutl.Text = decoded;
+
+            }
         }
     }
 }
